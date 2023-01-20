c1fdda0708c545ce9b80556b59f7195b4573a7f1
Christian Albert <christianalbert@users.noreply.github.com>
2023-01-20 18:15:20 +0100

    remove optional stuff

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -17,11 +17,5 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 source "$HOME/.cargo/env"
 
-rustup component add rustfmt
-rustup component add clippy 
-
-cargo install cargo-expand
-cargo install cargo-edit
-
 rustup update
 
