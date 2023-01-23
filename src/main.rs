113eb04cb2a719b3f2aa2b5e153b0f3e8b86cac8
Christian Albert <christianalbert@users.noreply.github.com>
2023-01-23 12:18:31 +0000

    some base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// fucntion that returns the square of a number
+fn square(x: i32) -> i32 {
+    x * x
+}
+
+// 
+// Path: src/lib.rs
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_square() {
+       assert_eq!(square(2), 4);
+       assert_eq!(square(3), 9);
+       assert_eq!(square(4), 16);
+       assert_eq!(square(5), 25);
+       assert_eq!(square(6), 36);
+       
+    }
+}
+
